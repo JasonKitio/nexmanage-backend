6393d7ba204382c41246f8e5917fc9975be990d6
bryan0-hit <loic.bryan@homesolutionsworld.org>
2025-06-25 09:21:35 +0100

    feat(conge): ajouter MessageGateway et AuthModule au module Conge

--- src/conge/conge.module.ts
+++ src/conge/conge.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MessageGateway } from 'src/message/message.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CongeService } from './conge.service';
 import { CongeController } from './conge.controller';
@@ -7,15 +8,16 @@ import { Utilisateur } from 'src/User/entities/utilisateur.entity';
 import { UtilisateurEntreprise } from 'src/UtilisateurEntreprise/entities/utilisateur-entreprise.entity';
 import { NotificationService } from './notification.service';
 import { MessageModule } from 'src/message/message.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Conge, Utilisateur, UtilisateurEntreprise]),
-    MessageModule,
+    MessageModule,AuthModule
     
   ],
   controllers: [CongeController],
-  providers: [CongeService, NotificationService],
-  exports: [CongeService],
+  providers: [CongeService, NotificationService,MessageGateway],
+  exports: [CongeService,MessageGateway],
 })
 export class CongeModule {}
